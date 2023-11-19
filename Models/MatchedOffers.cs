98ceac31e71fdd9ab482697b0346babb6002fdf4
mschinzvende <munyaldo2@gmail.com>
2023-11-20 00:23:58 +0200

    Working on DriverOffers

---
+++ Models/MatchedOffers.cs
@@ -0,0 +1,27 @@
+﻿namespace p2pRideshare.Models
+{
+    public class MatchedOffers
+    {
+        public string fullName = "";
+        public string idNo = "";
+        public string idScan = "";
+        public string profilePic = "";
+        public string phone = "";
+        public string email = "";
+        public string physicalAddress = "";
+
+        public string pickupLocation = "";
+        public string finalDestination = "";
+        public string vehicleRegNo = "";
+        public string vehicleMake = "";
+        public string vehicleColor = "";
+        public string picDriversLicense = "";
+        public string picZinaraReg = "";
+        public string pickupTime = "";
+        public string pickupDate = "";
+
+        public string matchId = "";
+        public string passengerStatus = "";
+        public string driverStatus = "";
+    }
+}
